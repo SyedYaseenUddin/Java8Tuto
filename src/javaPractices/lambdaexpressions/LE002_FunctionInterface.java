0ab4c4e44f9ac2e52ce446245879b814b4e95691
Syed Yaseen Uddin <57957691+SyedYaseenUddin@users.noreply.github.com>
2019-11-28 19:33:21 +0530

    Updated LE002_FunctionInterface-28-Nov-2019 19:32
    Comment added

--- src/javaPractices/lambdaexpressions/LE002_FunctionInterface.java
+++ src/javaPractices/lambdaexpressions/LE002_FunctionInterface.java
@@ -8,7 +8,7 @@ package javaPractices.lambdaexpressions;
  *
  */
 public class LE002_FunctionInterface {
-	
+	// Overriding Arthmetic2Value's operation method without using Anonymous class.
 	public Arthmetic2Value add = (a,b) ->  a + b;
 	public Arthmetic2Value sub = (a,b) ->  a - b;
 	public Arthmetic2Value mul = (a,b) ->  a * b;
